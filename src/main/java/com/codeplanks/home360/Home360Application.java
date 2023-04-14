dcf53ca9f8ffde541916b7909d89806b78f8a61e
Wasiu Idowu <hoxtygen@live.com>
2023-04-14 17:34:08 +0100

    Initial commit

---
+++ src/main/java/com/codeplanks/home360/Home360Application.java
@@ -0,0 +1,13 @@
+package com.codeplanks.home360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Home360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Home360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeplanks/home360/Home360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeplanks.home360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Home360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
