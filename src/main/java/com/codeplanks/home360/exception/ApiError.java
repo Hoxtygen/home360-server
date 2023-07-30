256eb3eb0de5a55e3ba63dea67010844e1fa9900
Wasiu Idowu <hoxtygen@live.com>
2023-07-30 08:03:20 +0100

    Clean up file

--- src/main/java/com/codeplanks/home360/exception/ApiError.java
+++ src/main/java/com/codeplanks/home360/exception/ApiError.java
@@ -19,30 +19,5 @@ public class ApiError {
   private LocalDateTime timestamp;
   private HttpStatus status;
   private String message;
-  private  List errors;
-//  private String debugMessage;
-//  private List<ApiSubError> subErrors;
-
-//  private  ApiError() {
-//    timestamp = LocalDateTime.now();
-//  }
-//
-//  ApiError(HttpStatus status) {
-//    this();
-//    this.status = status;
-//  }
-//
-//  ApiError(HttpStatus status, Throwable ex) {
-//    this();
-//    this.status = status;
-//    this.message = "Unexpected error";
-//    this.debugMessage = ex.getLocalizedMessage();
-//  }
-//
-//  ApiError(HttpStatus status, String message, Throwable ex) {
-//    this();
-//    this.status = status;
-//    this.message = message;
-//    this.debugMessage = ex.getLocalizedMessage();
-//  }
+  private  List<String> errors;
 }
