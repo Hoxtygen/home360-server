727ba6e575f12f6ac9a2c8c14e3ecc35d984e7a6
Wasiu Idowu <hoxtygen@live.com>
2023-11-18 16:38:00 +0100

    Remove unused import

--- src/main/java/com/codeplanks/home360/config/SecurityConfiguration.java
+++ src/main/java/com/codeplanks/home360/config/SecurityConfiguration.java
@@ -14,12 +14,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
 
 
