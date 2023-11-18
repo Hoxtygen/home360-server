8361d26a2f8807312d68189d2560600b792d7f7d
Wasiu Idowu <hoxtygen@live.com>
2023-11-18 15:10:46 +0100

    Add verification token entity and repository

---
+++ src/main/java/com/codeplanks/home360/auth/token/VerificationToken.java
@@ -0,0 +1,56 @@
+package com.codeplanks.home360.auth.token;
+
+
+import com.codeplanks.home360.auth.AuthenticationResponse;
+import com.codeplanks.home360.user.AppUser;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+
+/**
+ * @author Wasiu Idowu
+ */
+@Entity(name = "verification_tokens")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerificationToken {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private String token;
+  private Date expirationTime;
+  private static final int EXPIRATION_TIME = 15;
+
+  @OneToOne
+  @JoinColumn(name = "user_id")
+  private AppUser user;
+
+  public VerificationToken(String token, AppUser user) {
+    super();
+    this.token = token;
+    this.user = user;
+    this.expirationTime = this.getTokenExpirationTime();
+  }
+
+  public VerificationToken(String token) {
+    super();
+    this.token = token;
+    this.expirationTime = this.getTokenExpirationTime();
+  }
+
+  public Date getTokenExpirationTime() {
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTimeInMillis(new Date().getTime());
+    calendar.add(Calendar.MINUTE, EXPIRATION_TIME);
+    return new Date(calendar.getTime().getTime());
+  }
+}
