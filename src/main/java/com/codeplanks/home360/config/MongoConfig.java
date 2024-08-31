f0b5eafc918fc9c955343e823377130a1f37c834
Wasiu Idowu <hoxtygen@live.com>
2024-08-31 11:07:41 +0100

    chore: remove unused import

--- src/main/java/com/codeplanks/home360/config/MongoConfig.java
+++ src/main/java/com/codeplanks/home360/config/MongoConfig.java
@@ -1,7 +1,6 @@
 package com.codeplanks.home360.config;
 
 import com.codeplanks.home360.utils.ArrayListToStringConverter;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
@@ -9,7 +8,6 @@ import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import java.util.List;
 
 @Configuration
-//@EnableAutoConfiguration(exclude = {AutoConf})
 public class MongoConfig {
   @Bean
   public MongoCustomConversions mongoCustomConversions() {
