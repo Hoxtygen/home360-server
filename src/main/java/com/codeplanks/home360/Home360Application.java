79ad597097f68278709271bf05d6ef791d10c529
Wasiu Idowu <hoxtygen@live.com>
2024-09-18 21:36:30 +0100

    chore: update Main entry class with swagger documentation information.

--- src/main/java/com/codeplanks/home360/Home360Application.java
+++ src/main/java/com/codeplanks/home360/Home360Application.java
@@ -1,23 +1,19 @@
 package com.codeplanks.home360;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.ZoneId;
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @SpringBootApplication
 @RestController
 @EnableAsync(proxyTargetClass = true)
+@OpenAPIDefinition(
+    info = @Info(title = "Swagger API", version = "v1.0", description = "Home360 API"))
 public class Home360Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Home360Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Home360Application.class, args);
+  }
 }
