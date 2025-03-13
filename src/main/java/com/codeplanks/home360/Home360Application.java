5ec5547003e04b0096c39b5fed05630f36d44d09
Wasiu Idowu <hoxtygen@live.com>
2025-03-13 21:16:14 +0100

    fixing database connection error
    Not finding url for jdbc connection.

--- src/main/java/com/codeplanks/home360/Home360Application.java
+++ src/main/java/com/codeplanks/home360/Home360Application.java
@@ -4,6 +4,7 @@ package com.codeplanks.home360;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAsync(proxyTargetClass = true)
 @EnableTransactionManagement
 @EnableCaching
+@EnableJpaRepositories(basePackages = "com.codeplanks.home360.repository")
 public class Home360Application {
   public static void main(String[] args) {
     SpringApplication.run(Home360Application.class, args);
