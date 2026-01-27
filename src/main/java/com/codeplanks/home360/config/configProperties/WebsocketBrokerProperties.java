c4292ca0823e41afdd5f65b2d17725499bc11cb7
Wasiu Idowu <hoxtygen@live.com>
2026-01-27 18:51:26 +0100

    Upgrade spring boot to 3.2.5 and to jdk 21

---
+++ src/main/java/com/codeplanks/home360/config/configProperties/WebsocketBrokerProperties.java
@@ -0,0 +1,4 @@
+package com.codeplanks.home360.config.configProperties;
+
+public record WebsocketBrokerProperties() {
+}
---
+++ src/main/java/com/codeplanks/home360/utils/AppConstants.java
@@ -0,0 +1,13 @@
+package com.codeplanks.home360.utils;
+
+public final class Constants {
+
+  private Constants() {
+    throw new UnsupportedOperationException("Constant class cannot be instantiated");
+  }
+
+  public static final String WS_ENDPOINT = "/ws";
+  public static final String APP_PREFIX = "/app";
+  public static final String TOPIC_PREFIX = "/topic";
+
+}
